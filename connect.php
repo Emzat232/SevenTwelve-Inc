b9053609ea658d13e65d54a0ed123455f6a902b7
Emzat232 <161577048+Emzat232@users.noreply.github.com>
2025-04-27 14:04:22 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $host="localhost";
 $user="root";
 $pass="";
-$db="seventwelve"
+$db="login"
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error) {
   echo "Failed to connect to Database".$conn->connect_error;
